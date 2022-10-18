a569b35d40a18ec62f0c3a5e0c591709df74c864
toMilanesi <tomi.milanesi@gmail.com>
2022-10-18 08:50:18 -0300

    Add app component, add greeting component

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,13 +2,17 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './components/app';
 
-function App() {
-  return (<h1>Hello World!</h1>);
-}
+const append = document.getElementById('root');
+const root = createRoot(append)
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
-);
+root.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+)
--- app/javascript/components/app.js
+++ app/javascript/components/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Greeting from './greeting'
 
 function App() {
   return (
